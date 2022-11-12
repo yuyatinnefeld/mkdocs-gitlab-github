b370e1e139085cacdbe85c18c5fac23fa8df7d05
yuyatinnefeld <yuyawamo@gmail.com>
2022-11-12 10:08:32 +0100

    faet: update src with datalayer example

---
+++ src/datalayer/checkout_button.py
@@ -0,0 +1,25 @@
+
+def checkout_button():
+    """ Checkout Button DataLayer    
+    Returns:
+        checkout_button_dl (json): This is Checkout Button DataLayer Object
+    
+    """
+    
+    checkout_button_dl = {
+        event: "checkout_button",
+        gtm: {
+            uniqueEventId: 2,
+            start: 1639524976560,
+            scrollThreshold: 90,
+            scrollUnits: "percent",
+            scrollDirection: "vertical",
+            triggers: "1_27"
+        },
+        value: "120"
+    }
+    
+    return checkout_button_dl
+
+
+
